d2322395b7b981bb3f4b7d892b491d8007aa12ab
Matheus Vieira <matheusvieiradepaula@gmail.com>
2019-07-08 09:47:18 -0300

    Fix mobile issues

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 
 import { Message } from '../message';
 import { MessagesService } from '../messages.service';
+import { MatDrawer } from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -36,7 +37,12 @@ export class DashboardComponent implements OnInit {
     this.showMessage(this.messages[0].id)
   }
 
-  showMessage(messageId: number) {
+  showMessage(messageId: number, drawer?: MatDrawer) {
+    this.isHandset$.subscribe(result => {
+      if(result && drawer){
+        drawer.close();
+      }
+    });
     this.currentMessage = messageId;
   }
 
